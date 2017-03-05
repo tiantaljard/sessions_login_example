c67145649b2c026fe05663a5014faca14fe24293
TianTaljard <151172@rgu.ac.uk>
2017-03-05 14:23:59 +0000

    dr

--- homepage.php
+++ homepage.php
@@ -4,14 +4,16 @@ session_start();
 $access_level = $_COOKIE['access_level'];
 $username = $_COOKIE['username'];
 
-displayAccessLevelInformation($access_level);
+displayAccessLevelInformation($access_level,$username);
 
-function displayAccessLevelInformation($access_level)
+function displayAccessLevelInformation($access_level,$username)
 {
     if ($access_level == "standarduser") {
-        echo " <p>You are currently logged in as a standard user </p>";
+        echo " <p>You are currently logged in as a {$access_level} user </p>";
+        echo " <p>Your user name is {$username} </p>";
     } elseif ($access_level == "root") {
-        echo "<p>You are currently logged in as a root user</p>";
+        echo "<p>You are currently logged in as a {$access_level} user</p>";
         echo "<p>You now have access to additional administrative features</p>";
+        echo " <p>Your user name is {$username} </p>";
     }
 }
