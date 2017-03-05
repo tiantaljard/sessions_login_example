518ff2873cc37f91d5856f06dbbe461d98100c61
TianTaljard <151172@rgu.ac.uk>
2017-03-05 14:17:33 +0000

    tian t

--- homepage.php
+++ homepage.php
@@ -6,12 +6,14 @@ $username = $_COOKIE['username'];
 
 displayAccessLevelInformation($access_level);
 
-function displayAccessLevelInformation($access_level)
+function displayAccessLevelInformation($access_level,$username)
 {
     if ($access_level == "standarduser") {
-        echo " <p>You are currently logged in as a standard user </p>";
+        echo " <p>You are currently logged in as a {$access_level} user </p>";
+        echo " <p>Your user name is {$username} </p>";
     } elseif ($access_level == "root") {
-        echo "<p>You are currently logged in as a root user</p>";
+        echo "<p>You are currently logged in as a {$access_level} user</p>";
         echo "<p>You now have access to additional administrative features</p>";
+        echo " <p>Your user name is {$username} </p>";
     }
 }
