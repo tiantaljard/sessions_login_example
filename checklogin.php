ff35c889959a134b9630f0efa7ac6e29191a96c7
TianTaljard <151172@rgu.ac.uk>
2017-03-05 10:58:27 +0000

    dr

--- checklogin.php
+++ checklogin.php
@@ -5,25 +5,18 @@
  * Date: 05/03/2017
  * Time: 09:17
  */
+$username="tian";
+$password="taljard";
 
-/* These are our valid username and passwords */
-$user = 'tian';
-$pass = 'taljard';
+$username=$_POST["name"];
+$password=$_POST["password"];
 
-if (isset($_POST['username']) && isset($_POST['password'])) {
+if ($username=="username" && $password=="password")
+{
+    setcookie('access_level','standarduser');
 
-    if (($_POST['username'] == $user) && ($_POST['password'] == $pass)) {
-
-            setcookie('username', $_POST['username'] );
-            setcookie (access_level,'standarduser');
-
-        header('Location: index.php');
-
-    } else {
-        echo 'Username/Password Invalid';
-    }
-
-} else {
-    echo 'You must supply a username and password.';
 }
+
+header('Location: loggedIn.php')
+
 ?>
